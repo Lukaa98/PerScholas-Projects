2e327340dac1b6351d84e92ca22bb03612865abd
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2022-01-20 17:52:30 -0500

    delete

--- SpringBootRestfulCRUD/src/main/java/com/onlyxcodes/app/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.onlyxcodes.app.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.onlyxcodes.app.model.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Integer>{
-
-}
